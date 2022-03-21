c99dbc3b918a35f005cbf0b381a08670b5ae3d27
Anna <annavidaltur@gmail.com>
2022-03-21 21:59:00 +0100

    layout básico

--- app.R
+++ app.R
@@ -1,58 +1,24 @@
 library(shiny)
 
-# Define UI for app that draws a histogram ----
+# Define UI ----
 ui <- fluidPage(
+  titlePanel(h1("Paleta de colores")),
   
-  # App title ----
-  titlePanel("Hello World!"),
-  
-  # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 5,
-                  max = 50,
-                  value = 30)
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
+    sidebarPanel(h2("Subir imagen", align = "center"),
+                 fileInput("file", h3("Subir imagen")),
+                 submitButton("Analizar"),
+                ),
+    mainPanel(h2("Resultado", align = "center"),
+              img(src = "image.jpg", width=300)
+              )
   )
 )
 
-# Define server logic required to draw a histogram ----
+# Define server logic ----
 server <- function(input, output) {
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#75AADB", border = "orange",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
-  })
   
 }
 
+# Run the app ----
 shinyApp(ui = ui, server = server)
