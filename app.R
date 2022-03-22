53066dc6bf29561d4a9f7f6052d1d6dfddaf9b61
Anna <annavidaltur@gmail.com>
2022-03-22 20:29:38 +0100

    display image uploaded, save it in server and show random plot

--- app.R
+++ app.R
@@ -1,24 +1,59 @@
 library(shiny)
+library(base64enc)
+library(imager)
 
-# Define UI ----
+
 ui <- fluidPage(
-  titlePanel(h1("Paleta de colores")),
+  fileInput("myFile", "Upload image", accept = c("image/png", "image/jpg")),
+  h2('Image uploaded:'),
+  uiOutput("image"),
   
-  sidebarLayout(
-    sidebarPanel(h2("Subir imagen", align = "center"),
-                 fileInput("file", h3("Subir imagen")),
-                 submitButton("Analizar"),
-                ),
-    mainPanel(h2("Resultado", align = "center"),
-              img(src = "image.jpg", width=300)
-              )
-  )
+  h2('Palette:'),
+  plotOutput("plotPalette"),
 )
 
-# Define server logic ----
-server <- function(input, output) {
+server <- function(input, output){
+  # Copy image uploaded to www/
+  observeEvent(input$myFile, {
+    inFile <- input$myFile
+    if (is.null(inFile))
+      return()
+    file.copy(inFile$datapath, file.path("www", inFile$name) )
+  })
   
+  # Show image uploaded
+  base64 <- reactive({
+    inFile <- input[["myFile"]]
+    if(!is.null(inFile)){
+      dataURI(file = inFile$datapath, mime = "image/png")
+    }
+  })
+  
+  # Show image uploaded
+  output[["image"]] <- renderUI({
+    if(!is.null(base64())){
+      tags$div(
+        tags$img(src= base64(), width="100%"),
+        style = "width: 400px;"
+      )
+    }
+  })
+  
+  # Create palette
+  output$plotPalette <- renderImage({
+    # A temp file to save the output. It will be deleted after renderImage
+    # sends it, because deleteFile=TRUE.
+    outfile <- tempfile(fileext='.png')
+    
+    # Generate a png
+    png(outfile, width=400, height=400)
+    plot(boats)
+    dev.off()
+    
+    # Return a list
+    list(src = outfile,
+         alt = "This is alternate text")
+  }, deleteFile = FALSE)
 }
 
-# Run the app ----
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
